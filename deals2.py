f6f513020e2e78c47f17f0bc7c1919a7a3ce769e
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2021-12-05 13:26:34 +0000

    first website deals complete

--- deals2.py
+++ deals2.py
@@ -8,18 +8,20 @@ service = Service(ChromeDriverManager().install())
 browser2 = webdriver.Chrome(service=service)
 browser2.get("https://www.bt.com/products/broadband/deals")
 
-browser2.implicitly_wait(30)
+browser2.implicitly_wait(5)
 
-browser2.send_keys()
-browser2.send_keys(Keys)
+# browser2.send_keys()
+# browser2.send_keys(Keys)
 
 # accept_cookies = browser2.find_element(
 #     By.XPATH, "/html/body/div[8]/div[1]/div/div[3]/a[1]"
 # )
 
-accept_cookies = browser2.find_elements(By.LINK_TEXT, "Accept all cookies")
-# print(accept_cookies.text)
+# accept_cookies = browser2.find_element(By.LINK_TEXT, "Accept all cookies")
+# print(accept_cookies)
 # accept_cookies.click()
+accept_cookies = browser2.find_elements(By.CLASS_NAME, "pdynamicbutton")
+print(accept_cookies)
 
 
 # deal name
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pprint
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -11,49 +13,42 @@ browser1.get("https://www.hyperoptic.com/offer/")
 accept_cookies = browser1.find_element(
     By.XPATH, '//*[@id="cookiePolicyModal"]/div/div/div[3]/button[1]'
 )
-
 accept_cookies.click()
 
 browser1.implicitly_wait(30)
 
+deals = []
 # deal name
 deal_name = browser1.find_elements(By.CLASS_NAME, "size-unit",)
-print("_________")
-print("Deal name")
-print("_________")
 for name in deal_name:
-    print(name.text + " " + "deal\n")
+    deals.append({"name": name.text})
 
 # deal price
-deal_price = browser1.find_elements(By.CLASS_NAME, "price",)
-print("__________")
-print("Deal price")
-print("__________")
-for price in deal_price:
-    print(price.text + " " + "per month\n")
+deal_prices = browser1.find_elements(By.CLASS_NAME, "price",)
+for index, price in enumerate(deal_prices):
+    deals[index]["price"] = price.text
 
 
 # deal speed & setup cost
 deal_speed_and_setup = browser1.find_elements(
     By.CLASS_NAME, "flat-text-bellow-price-wr",
 )
-print("_______________________")
-print("Deal speed & setup cost")
-print("_______________________")
-for speed_and_cost in deal_speed_and_setup:
-    print(speed_and_cost.text + "\n")
+
+for index, speed_and_cost in enumerate(deal_speed_and_setup):
+    deals[index]["speed"] = speed_and_cost.text.splitlines()[1]
+    deals[index]["setup cost"] = speed_and_cost.text.splitlines()[3]
 
 
 # deal contract length
 deal_contract_length = browser1.find_elements(
-    By.XPATH, '//*[@id="block_61711b89d05de"]/div/div[3]/div/div[2]/div[1]/label[1]'
+    By.CLASS_NAME, "bold-text-bellow-price-wr"
 )
-print("____________________")
-print("Deal contract length")
-print("____________________")
-for contract_length in deal_contract_length:
-    e = 1
-    while e <= 4:
-        print(contract_length.text + "\n")
-        e += 1
+print("_____")
+print("Deals")
+print("_____")
+for index, contract in enumerate(deal_contract_length):
+    deals[index]["contract"] = contract.text
 
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(deals)
